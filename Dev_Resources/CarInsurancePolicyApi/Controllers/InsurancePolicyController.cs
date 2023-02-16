2f1b8bf906f9d988ab8387f82bc2540264e03c16
Sebastian Agudelo Betancur <sebasbeta@MacBook-Pro-de-Sebastian.local>
2023-02-15 19:09:17 -0500

    Test personalsoft

---
+++ Dev_Resources/CarInsurancePolicyApi/Controllers/InsurancePolicyController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using CarInsurancePolicyService.Services;
+using CarInsurancePolicyContracts.Requests;
+using CarInsurancePolicyDomain.Exceptions;
+
+namespace CarInsurancePolicyPresentation.V1.Controllers
+{
+    [Route("api/v1/[controller]/")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class InsurancePolicyController : ControllerBase
+    {
+        private readonly ICarInsuranceServices _carInsuranceServices;
+        
+
+        public InsurancePolicyController(ICarInsuranceServices carInsuranceServices)
+        {
+            _carInsuranceServices = carInsuranceServices;
+        }
+
+        [HttpPost]
+        [Route("SaveCarInsurancePolicy")]
+        public async Task<IActionResult> SaveCarInsurancePolicy(CarInsurancePolicyRequest carInsurancePolicyRequest)
+        {
+            try
+            {
+                var response = await _carInsuranceServices.SaveCarInsurance(carInsurancePolicyRequest);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                if(ex is BadRequestException)
+                {
+                    throw;
+                }
+
+                return BadRequest(ex.Message);
+            }
+           
+        }
+
+        [HttpGet]
+        [Route("GetCarInsurancePolicy/{filter]")]
+        public async Task<IActionResult> GetCarInsurancePolicy(string filter)
+        {
+            try
+            {
+                var response = await _carInsuranceServices.GetPolicyByFilter(filter);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                if (ex is BadRequestException)
+                {
+                    throw;
+                }
+
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+}
+
---
+++ Dev_Resources/CarInsurancePolicyApi/Controllers/ugyLoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using CarInsurancePolicyService.Services;
+using CarInsurancePolicyContracts.Requests;
+using CarInsurancePolicyDomain.Exceptions;
+
+namespace CarInsurancePolicyPresentation.V1.Controllers
+{
+    [Route("api/v1/[controller]/")]
+    [ApiController]
+    [AllowAnonymous]
+    public class LoginController : ControllerBase
+    {
+        private readonly ITokenJwt _tokenJwt;
+
+        public LoginController(ITokenJwt tokenJwt)
+        {
+            _tokenJwt = tokenJwt;
+        }
+
+        [HttpPost]
+        [Route("LoginUser")]
+        public ActionResult LoginUser(LoginRequest loginRequest)
+        {
+            try
+            {
+                var response =  _tokenJwt.GetJwtAsync(loginRequest);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                if (ex is BadRequestException)
+                {
+                    throw;
+                }
+
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
+
